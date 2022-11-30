4f2de4bc54c3c404aed51adddd518e5638f31d6a
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2022-11-30 16:02:21 +0100

    Implemented new multi_target_regression
    code based on @ahendriksen

--- rapids_singlecell/cunnData_funcs/_regress_out.py
+++ rapids_singlecell/cunnData_funcs/_regress_out.py
@@ -3,7 +3,9 @@ import cupyx as cpx
 from cuml.linear_model import LinearRegression
 from ..cunnData import cunnData
 
-def regress_out(cudata:cunnData, keys, verbose=False):
+import math
+
+def regress_out(cudata, keys, batchsize = None, regress_all = True, verbose=False):
 
     """
     Use linear regression to adjust for the effects of unwanted noise
@@ -15,7 +17,18 @@ def regress_out(cudata:cunnData, keys, verbose=False):
         The annotated data matrix.
     keys
         Keys for numerical observation annotation on which to regress on.
-
+    
+    batchsize: int (default: None)
+        Number of genes that should be processed together. 
+        If `None` each gene will be analysed seperatly.
+        If `.n_obs` < 100000 all genes will be processed together.
+        Will be ignored if cuML version < 22.12
+        
+    regress_all: bool (default: True)
+        Weather or not you want to process all regressions together.
+        Will be ignored if `.n_obs` > 100000.
+        Will be ignored if cuML version < 22.12
+        
     verbose : bool
         Print debugging information
 
@@ -45,12 +58,32 @@ def regress_out(cudata:cunnData, keys, verbose=False):
     if cudata.X.shape[0] < 100000 and cpx.scipy.sparse.issparse(cudata.X):
         cudata.X = cudata.X.todense()
     
-    for i in range(cudata.X.shape[1]):
-        if verbose and i % 500 == 0:
-            print("Regressed %s out of %s" %(i, cudata.X.shape[1]))
+    ### Implemented new multi_target_regression based on code by @ahendriksen
+    cuml_supports_multi_target = LinearRegression._get_tags()['multioutput']
+
+    if cuml_supports_multi_target and not cp.sparse.issparse(cudata.X) and regress_all:
         X = regressors
-        y = cudata.X[:,i]
-        outputs[:, i] = _regress_out_chunk(X, y)
+        lr = LinearRegression(fit_intercept=False, output_type="cupy", algorithm='svd')
+        lr.fit(X, cudata.X, convert_dtype=True)
+        outputs[:] = cudata.X - lr.predict(X)
+    elif cuml_supports_multi_target and cp.sparse.issparse(cudata.X) and batchsize:
+        n_batches = math.ceil(cudata.X.shape[1] / batchsize)
+        for batch in range(n_batches):
+            start_idx = batch * batchsize
+            stop_idx = min(batch * batchsize + batchsize, cudata.X.shape[1])
+            arr_batch = cudata.X[:,start_idx:stop_idx].todense()
+            X = regressors
+            lr = LinearRegression(fit_intercept=False, output_type="cupy", algorithm='svd')
+            lr.fit(X, arr_batch, convert_dtype=True)
+            outputs[:,start_idx:stop_idx] =arr_batch - lr.predict(X)
+    else:
+        for i in range(cudata.X.shape[1]):
+            if verbose and i % 500 == 0:
+                print("Regressed %s out of %s" %(i, cudata.X.shape[1]))
+            X = regressors
+            y = cudata.X[:,i]
+            outputs[:, i] = _regress_out_chunk(X, y)
+            
     cudata.X = outputs
 
 def _regress_out_chunk(X, y):
