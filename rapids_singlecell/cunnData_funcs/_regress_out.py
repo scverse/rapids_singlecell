818bd4baa1f1f9af67673595a3e9335bd9a8b011
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2022-12-01 14:24:30 +0100

    new and better syntax for `regress_out`

--- rapids_singlecell/cunnData_funcs/_regress_out.py
+++ rapids_singlecell/cunnData_funcs/_regress_out.py
@@ -1,11 +1,15 @@
 import cupy as cp
 import cupyx as cpx
 from cuml.linear_model import LinearRegression
+from rapids_singlecell.cunnData import cunnData
+from typing import Literal, Union
 from ..cunnData import cunnData
-
 import math
 
-def regress_out(cudata, keys, batchsize = None, regress_all = True, verbose=False):
+def regress_out(cudata:cunnData,
+                keys,
+                batchsize: Union[int,Literal["all"],None] = 100,
+                verbose=False):
 
     """
     Use linear regression to adjust for the effects of unwanted noise
@@ -17,15 +21,10 @@ def regress_out(cudata, keys, batchsize = None, regress_all = True, verbose=Fals
     keys
         Keys for numerical observation annotation on which to regress on.
     
-    batchsize: int (default: None)
+    batchsize: Union[int,Literal["all"],None] (default: 100)
         Number of genes that should be processed together. 
+        If `'all'` all genes will be processed together if `.n_obs` <100000. 
         If `None` each gene will be analysed seperatly.
-        If `.n_obs` < 100000 all genes will be processed together.
-        Will be ignored if cuML version < 22.12
-        
-    regress_all: bool (default: True)
-        Weather or not you want to process all regressions together.
-        Will be ignored if `.n_obs` > 100000.
         Will be ignored if cuML version < 22.12
         
     verbose : bool
@@ -34,6 +33,9 @@ def regress_out(cudata, keys, batchsize = None, regress_all = True, verbose=Fals
     -------
     updates cunndata object with the corrected data matrix
     """
+    
+    if batchsize != "all" and type(batchsize) not in [int, type(None)]:
+        raise ValueError("batchsize must be `int`, `None` or `'all'`")
     
     if cpx.scipy.sparse.issparse(cudata.X) and not cpx.scipy.sparse.isspmatrix_csc(cudata.X):
         cudata.X = cudata.X.tocsc()
@@ -51,28 +53,34 @@ def regress_out(cudata, keys, batchsize = None, regress_all = True, verbose=Fals
 
     outputs = cp.empty(cudata.X.shape, dtype=cudata.X.dtype, order="F")
 
-    if cudata.X.shape[0] < 100000 and cpx.scipy.sparse.issparse(cudata.X) and not batchsize:
-        cudata.X = cudata.X.todense()
-    
-    # Implemented new multi_target_regression based on code by @ahendriksen
     cuml_supports_multi_target = LinearRegression._get_tags()['multioutput']
 
-    if cuml_supports_multi_target and not cp.sparse.issparse(cudata.X) and regress_all:
-        X = regressors
-        lr = LinearRegression(fit_intercept=False, output_type="cupy", algorithm='svd')
-        lr.fit(X, cudata.X, convert_dtype=True)
-        outputs[:] = cudata.X - lr.predict(X)
-    elif cuml_supports_multi_target and batchsize:
-        n_batches = math.ceil(cudata.X.shape[1] / batchsize)
-        for batch in range(n_batches):
-            start_idx = batch * batchsize
-            stop_idx = min(batch * batchsize + batchsize, cudata.X.shape[1])
-            arr_batch = cudata.X[:,start_idx:stop_idx].todense()
+    if cuml_supports_multi_target and batchsize:
+        if batchsize == "all" and cudata.X.shape[0] < 100000:
+            if cpx.scipy.sparse.issparse(cudata.X): 
+                cudata.X = cudata.X.todense()
             X = regressors
             lr = LinearRegression(fit_intercept=False, output_type="cupy", algorithm='svd')
-            lr.fit(X, arr_batch, convert_dtype=True)
-            outputs[:,start_idx:stop_idx] =arr_batch - lr.predict(X)
+            lr.fit(X, cudata.X, convert_dtype=True)
+            outputs[:] = cudata.X - lr.predict(X)
+        else:
+            if batchsize == "all":
+                batchsize = 100
+            n_batches = math.ceil(cudata.X.shape[1] / batchsize)
+            for batch in range(n_batches):
+                start_idx = batch * batchsize
+                stop_idx = min(batch * batchsize + batchsize, cudata.X.shape[1])
+                if cpx.scipy.sparse.issparse(cudata.X):
+                    arr_batch = cudata.X[:,start_idx:stop_idx].todense()
+                else:
+                    arr_batch = cudata.X[:,start_idx:stop_idx].copy()
+                X = regressors
+                lr = LinearRegression(fit_intercept=False, output_type="cupy", algorithm='svd')
+                lr.fit(X, arr_batch, convert_dtype=True)
+                outputs[:,start_idx:stop_idx] =arr_batch - lr.predict(X)
     else:
+        if cudata.X.shape[0] < 100000 and cpx.scipy.sparse.issparse(cudata.X):
+            cudata.X = cudata.X.todense()
         for i in range(cudata.X.shape[1]):
             if verbose and i % 500 == 0:
                 print("Regressed %s out of %s" %(i, cudata.X.shape[1]))
@@ -80,7 +88,7 @@ def regress_out(cudata, keys, batchsize = None, regress_all = True, verbose=Fals
             y = cudata.X[:,i]
             outputs[:, i] = _regress_out_chunk(X, y)
             
-    cudata.X = outputs
+    cudata.X= outputs
 
 def _regress_out_chunk(X, y):
     """
