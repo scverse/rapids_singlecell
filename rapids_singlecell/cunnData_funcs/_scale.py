0b711809275fece37952a99c5b8b5b460d5c8f0c
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2023-01-18 15:44:10 +0100

    added `.layer` and `inplace` support

--- rapids_singlecell/cunnData_funcs/_scale.py
+++ rapids_singlecell/cunnData_funcs/_scale.py
@@ -1,28 +1,52 @@
 import cupy as cp
 from ..cunnData import cunnData
+from typing import Optional
 
-def scale(cudata:cunnData, max_value=10):
+
+def scale(cudata:cunnData, 
+        max_value=10,
+        layer: Optional[str] = None,
+        inplace = True):
     """
     Scales matrix to unit variance and clips values
     Parameters
     ----------
+    cudata:
+        cunnData object
+
     max_value : int
-                After scaling matrix to unit variance,
-                values will be clipped to this number
-                of std deviations.
+        After scaling matrix to unit variance,
+        values will be clipped to this number
+        of std deviations.
+
+    layer : Optional[str] (default: None)
+        Layer to use as input instead of X. If None, X is used.
+
+    inplace : bool (default: True)
+        If True, update cunnData with results. Otherwise, return results. See below for details of what is returned.
     Return
     ------
-    updates cunndata object with a scaled cunndata.X
+    Returns a sacled copy or  updates `cudata` with a scaled version of the 
+    original `cudata.X` and `cudata.layers['layer']`, depending on `inplace`.
     """
-    if type(cudata.X) is not cp._core.core.ndarray:
+    X = cudata.layers[layer] if layer is not None else cudata.X
+
+    if type(X) is not cp._core.core.ndarray:
         print("densifying _.X")
-        X = cudata.X.toarray()
+        X = X.toarray()
     else:
-        X =cudata.X
-    mean = X.mean(axis=0)
+        X =X.copy()
+    mean = X.sum(axis=0).flatten() / X.shape[0]
     X -= mean
     del mean
     stddev = cp.sqrt(X.var(axis=0))
     X /= stddev
     del stddev
-    cudata.X = cp.clip(X,a_max=max_value)
+    X= cp.clip(X,a_max=max_value)
+    if inplace:
+        if layer:
+            cudata.layers[layer] = X
+        else:
+            cudata.X = X
+    else:
+        return X
