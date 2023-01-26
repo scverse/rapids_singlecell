3bb86a6023ee32a3b95265e8659569e848a6f6d9
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2023-01-26 16:53:34 +0100

    Delete rapids_singlecell directory

--- rapids_singlecell/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import cunnData
-from . import cunnData_funcs as pp
-from . import decoupler_gpu as dcg
-from . import scanpy_gpu as tl
-
-__version__ = '0.3.4'
--- rapids_singlecell/decoupler_gpu/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._method_mlm import run_mlm
-from ._method_wsum import run_wsum
