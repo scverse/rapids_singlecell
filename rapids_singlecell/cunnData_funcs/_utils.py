c7a2a2b6e69482273b61b55bce96d2844cdd16de
Intron7 <severin.dicks@icloud.com>
2023-05-14 17:39:19 +0200

    updated _mean_var_minor

--- rapids_singlecell/cunnData_funcs/_utils.py
+++ rapids_singlecell/cunnData_funcs/_utils.py
@@ -32,24 +32,17 @@ _get_mean_var_major = cp.RawKernel(
 _get_mean_var_minor = cp.RawKernel(
     r"""
     extern "C" __global__
-    void caluclate_mean_minor(const int *indptr,const int *index,const float *data,
+    void caluclate_mean_minor(const int *index,const float *data,
                         double* means, double* vars,
-                        int major, int minor) {
-        int major_idx = blockDim.x * blockIdx.x + threadIdx.x;
-        if(major_idx >= major){
+                        int major, int nnz) {
+        int idx = blockDim.x * blockIdx.x + threadIdx.x;
+        if(idx >= nnz){
             return;
         }
-        int start_idx = indptr[major_idx];
-        int stop_idx = indptr[major_idx+1];
-
-        for(int minor_idx = start_idx; minor_idx < stop_idx; minor_idx++){
-               double value = (double) data[minor_idx];
-               int minor_pos = index[minor_idx];
-               atomicAdd(&means[minor_pos], value/major);
-               atomicAdd(&vars[minor_pos], value*value/major);
-        }
-
-
+       double value = (double) data[idx];
+       int minor_pos = index[idx];
+       atomicAdd(&means[minor_pos], value/major);
+       atomicAdd(&vars[minor_pos], value*value/major);
         }
     """,
     "caluclate_mean_minor",
@@ -72,10 +65,8 @@ def _mean_var_minor(X, major, minor):
     mean = cp.zeros(minor, dtype=cp.float64)
     var = cp.zeros(minor, dtype=cp.float64)
     block = (32,)
-    grid = (int(math.ceil(major / block[0])),)
-    _get_mean_var_minor(
-        grid, block, (X.indptr, X.indices, X.data, mean, var, major, minor)
-    )
+    grid = (int(math.ceil(X.nnz / block[0])),)
+    _get_mean_var_minor(grid, block, (X.indices, X.data, mean, var, major, X.nnz))
 
     var = (var - mean**2) * (major / (major - 1))
     return mean, var
