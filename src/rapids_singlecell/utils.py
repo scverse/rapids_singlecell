60baa7881eeef24b32bb6d157f93afb7d6773515
Intron7 <severin.dicks@icloud.com>
2023-08-23 16:40:49 +0200

    adds utils

---
+++ src/rapids_singlecell/utils.py
@@ -0,0 +1,19 @@
+from anndata import AnnData
+
+
+def anndata_to_GPU(
+    adata: AnnData,
+):
+    """
+    anndata to VRAM
+    """
+    pass
+
+
+def anndata_to_CPU(
+    adata: AnnData,
+):
+    """
+    anndata to RAM
+    """
+    pass
