df420380f02d386ec417825c76679dcd8390f2d0
ilan-gold <ilanbassgold@gmail.com>
2024-07-01 09:58:41 +0200

    (fix): initialization

--- tests/dask/test_dask_pca.py
+++ tests/dask/test_dask_pca.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import cupy as cp
 import numpy as np
 from scipy import sparse
+from cupyx.scipy import sparse as cusparse
 from conftest import as_dense_cupy_dask_array, as_sparse_cupy_dask_array
 import rapids_singlecell as rsc
 
@@ -37,7 +38,7 @@ def test_pca_sparse_dask(client):
 def test_pca_sparse_dask_full_pipeline(client):
     sparse_ad = pbmc3k_processed()
     default = pbmc3k_processed()
-    sparse_ad.X = sparse.csr_matrix(sparse_ad.X.astype(np.float64))
+    sparse_ad.X = cusparse.csr_matrix(sparse.csr_matrix(sparse_ad.X.astype(np.float64)))
     default.X = as_sparse_cupy_dask_array(default.X.astype(np.float64))
 
     rsc.pp.normalize_total(sparse_ad,  target_sum=1e4)
