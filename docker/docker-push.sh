a527d8ec15a6b4786ac8b8ce1e804bf5391aa7f9
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2024-08-09 13:17:58 +0200

    rapids-24.08 (#241)
    * update rapids
    
    * updates release notes

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 docker_account=scverse
-rapids_version=24.06
+rapids_version=24.08
 grep -v -- '- rapids-singlecell' conda/rsc_rapids_${rapids_version}.yml > rsc_rapids.yml
 docker build -t rapids-singlecell-deps:latest -f docker/Dockerfile.deps .
 rm rsc_rapids.yml
