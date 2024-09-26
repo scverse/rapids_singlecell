8ebce65d2227c6cd316a1c7ca95579e7b42da42a
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2024-09-26 16:26:39 +0200

    Add BBKNN (#264)
    * add bbknn
    
    * add test
    
    * updates docs
    
    * add docstring
    
    * Update pyproject.toml
    
    Co-authored-by: Philipp A. <flying-sheep@web.de>
    
    * update neighbors
    
    * add releasenote for neighbors
    
    * update test
    
    * add annotation
    
    * switch batch confirmation
    
    * update test wording
    
    * Update src/rapids_singlecell/preprocessing/_neighbors.py
    
    Co-authored-by: Philipp A. <flying-sheep@web.de>
    
    * Format docs
    
    ---------
    
    Co-authored-by: Philipp A. <flying-sheep@web.de>

--- src/rapids_singlecell/preprocessing/__init__.py
+++ src/rapids_singlecell/preprocessing/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from ._harmony_integrate import harmony_integrate
 from ._hvg import highly_variable_genes
-from ._neighbors import neighbors
+from ._neighbors import bbknn, neighbors
 from ._normalize import log1p, normalize_pearson_residuals, normalize_total
 from ._pca import pca
 from ._regress_out import regress_out
--- tests/test_neighbors.py
+++ tests/test_neighbors.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+import itertools
+
 import numpy as np
 import pytest
+import scanpy.external as sce
 from anndata import AnnData
 from scanpy.datasets import pbmc68k_reduced
 
-from rapids_singlecell.pp import neighbors
+from rapids_singlecell.pp import bbknn, neighbors
 
 # the input data
 X = np.array([[1, 0], [3, 0], [5, 6], [0, 4]])
@@ -52,3 +55,32 @@ def test_neighbors_key_added():
     assert np.allclose(
         adata.obsp["distances"].toarray(), adata.obsp[dists_key].toarray()
     )
+
+
+def test_bbknn():
+    """
+    Test the bbknn function against the scanpy implementation. We want more than a 90% overlap between the two.
+    This is calculated by the number of shared indices between the two sparse distance matrices divided by the number of indices in the original implementation.
+    """
+
+    adata = pbmc68k_reduced()
+    sce.pp.bbknn(
+        adata,
+        n_pcs=15,
+        batch_key="phase",
+        computation="pynndescent",
+        metric="euclidean",
+    )
+    bbknn(adata, n_pcs=15, batch_key="phase", algorithm="brute", key_added="rapids")
+    counter = 0
+    for (rsc_start, rsc_stop), (b_start, b_stop) in zip(
+        itertools.pairwise(adata.obsp["rapids_distances"].indptr),
+        itertools.pairwise(adata.obsp["distances"].indptr),
+    ):
+        counter += len(
+            np.intersect1d(
+                adata.obsp["rapids_distances"].indices[rsc_start:rsc_stop],
+                adata.obsp["distances"].indices[b_start:b_stop],
+            )
+        )
+    assert counter / b_stop > 0.9
