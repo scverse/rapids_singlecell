65f941ae88eac35c180a9e4bcfb2902534d65d64
Intron7 <severin.dicks@icloud.com>
2024-10-16 15:17:44 +0200

    compress hvg tests

--- tests/dask/test_hvg_dask.py
+++ tests/dask/test_hvg_dask.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import cupy as cp
 import pandas as pd
+import pytest
 import scanpy as sc
 from cupyx.scipy import sparse as cusparse
 from scanpy.datasets import pbmc3k
@@ -22,13 +23,25 @@ def _get_anndata():
     return adata
 
 
-def test_seurat_sparse(client):
+@pytest.mark.parametrize("data_kind", ["sparse", "dense"])
+@pytest.mark.parametrize("flavor", ["seurat", "cell_ranger"])
+def test_highly_variable_genes(client, data_kind, flavor):
     adata = _get_anndata()
+    adata.X = adata.X.astype("float64")
     dask_data = adata.copy()
-    dask_data.X = as_sparse_cupy_dask_array(dask_data.X).persist()
-    adata.X = cusparse.csr_matrix(adata.X)
-    rsc.pp.highly_variable_genes(adata)
-    rsc.pp.highly_variable_genes(dask_data)
+
+    if data_kind == "dense":
+        dask_data.X = as_dense_cupy_dask_array(dask_data.X).persist()
+        adata.X = cp.array(adata.X.toarray())
+    elif data_kind == "sparse":
+        dask_data.X = as_sparse_cupy_dask_array(dask_data.X).persist()
+        adata.X = cusparse.csr_matrix(adata.X)
+    else:
+        raise ValueError(f"Unknown data_kind: {data_kind}")
+
+    rsc.pp.highly_variable_genes(adata, flavor=flavor)
+    rsc.pp.highly_variable_genes(dask_data, flavor=flavor)
+
     cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
     cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
     cp.testing.assert_allclose(
@@ -36,111 +49,24 @@ def test_seurat_sparse(client):
     )
 
 
-def test_seurat_sparse_batch(client):
+@pytest.mark.parametrize("data_kind", ["sparse", "dense"])
+@pytest.mark.parametrize("flavor", ["seurat", "cell_ranger"])
+def test_highly_variable_genes_batched(client, data_kind, flavor):
     adata = _get_anndata()
     adata.obs["batch"] = (
         "source_" + pd.array([*range(1, 6), 5]).repeat(500).astype("string")
     )[: adata.n_obs]
     dask_data = adata.copy()
-    dask_data.X = as_sparse_cupy_dask_array(dask_data.X).persist()
-    adata.X = cusparse.csr_matrix(adata.X)
-    rsc.pp.highly_variable_genes(adata, batch_key="batch")
-    rsc.pp.highly_variable_genes(dask_data, batch_key="batch")
-    cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
-    cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
-    cp.testing.assert_allclose(
-        adata.var["dispersions_norm"], dask_data.var["dispersions_norm"]
-    )
 
+    if data_kind == "dense":
+        dask_data.X = as_dense_cupy_dask_array(dask_data.X).persist()
+        adata.X = cp.array(adata.X.toarray())
+    elif data_kind == "sparse":
+        dask_data.X = as_sparse_cupy_dask_array(dask_data.X).persist()
+        adata.X = cusparse.csr_matrix(adata.X)
+    else:
+        raise ValueError(f"Unknown data_kind: {data_kind}")
 
-def test_cr_sparse(client):
-    adata = _get_anndata()
-    dask_data = adata.copy()
-    dask_data.X = as_sparse_cupy_dask_array(dask_data.X).persist()
-    adata.X = cusparse.csr_matrix(adata.X)
-    rsc.pp.highly_variable_genes(adata, flavor="cell_ranger")
-    rsc.pp.highly_variable_genes(dask_data, flavor="cell_ranger")
-    cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
-    cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
-    cp.testing.assert_allclose(
-        adata.var["dispersions_norm"], dask_data.var["dispersions_norm"]
-    )
-
-
-def test_cr_sparse_batch(client):
-    adata = _get_anndata()
-    adata.obs["batch"] = (
-        "source_" + pd.array([*range(1, 6), 5]).repeat(500).astype("string")
-    )[: adata.n_obs]
-    dask_data = adata.copy()
-    dask_data.X = as_sparse_cupy_dask_array(dask_data.X).persist()
-    adata.X = cusparse.csr_matrix(adata.X)
-    rsc.pp.highly_variable_genes(adata, batch_key="batch", flavor="cell_ranger")
-    rsc.pp.highly_variable_genes(dask_data, batch_key="batch", flavor="cell_ranger")
-    cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
-    cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
-    cp.testing.assert_allclose(
-        adata.var["dispersions_norm"], dask_data.var["dispersions_norm"]
-    )
-
-
-def test_cr_dense(client):
-    adata = _get_anndata()
-    adata.X = adata.X.astype("float64")
-    dask_data = adata.copy()
-    dask_data.X = as_dense_cupy_dask_array(dask_data.X).persist()
-    adata.X = cp.array(adata.X.toarray())
-    rsc.pp.highly_variable_genes(adata, flavor="cell_ranger")
-    rsc.pp.highly_variable_genes(dask_data, flavor="cell_ranger")
-    cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
-    cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
-    cp.testing.assert_allclose(
-        adata.var["dispersions_norm"], dask_data.var["dispersions_norm"]
-    )
-
-
-def test_seurat_dense(client):
-    adata = _get_anndata()
-    adata.X = adata.X.astype("float64")
-    dask_data = adata.copy()
-    dask_data.X = as_dense_cupy_dask_array(dask_data.X).persist()
-    adata.X = cp.array(adata.X.toarray())
-    rsc.pp.highly_variable_genes(adata)
-    rsc.pp.highly_variable_genes(dask_data)
-    cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
-    cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
-    cp.testing.assert_allclose(
-        adata.var["dispersions_norm"], dask_data.var["dispersions_norm"]
-    )
-
-
-def test_cr_dense_batch(client):
-    adata = _get_anndata()
-    adata.obs["batch"] = (
-        "source_" + pd.array([*range(1, 6), 5]).repeat(500).astype("string")
-    )[: adata.n_obs]
-    adata.X = adata.X.astype("float64")
-    dask_data = adata.copy()
-    dask_data.X = as_dense_cupy_dask_array(dask_data.X).persist()
-    adata.X = cp.array(adata.X.toarray())
-    rsc.pp.highly_variable_genes(adata, batch_key="batch", flavor="cell_ranger")
-    rsc.pp.highly_variable_genes(dask_data, batch_key="batch", flavor="cell_ranger")
-    cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
-    cp.testing.assert_allclose(adata.var["dispersions"], dask_data.var["dispersions"])
-    cp.testing.assert_allclose(
-        adata.var["dispersions_norm"], dask_data.var["dispersions_norm"]
-    )
-
-
-def test_seurat_dense_batch(client):
-    adata = _get_anndata()
-    adata.obs["batch"] = (
-        "source_" + pd.array([*range(1, 6), 5]).repeat(500).astype("string")
-    )[: adata.n_obs]
-    adata.X = adata.X.astype("float64")
-    dask_data = adata.copy()
-    dask_data.X = as_dense_cupy_dask_array(dask_data.X).persist()
-    adata.X = cp.array(adata.X.toarray())
     rsc.pp.highly_variable_genes(adata, batch_key="batch")
     rsc.pp.highly_variable_genes(dask_data, batch_key="batch")
     cp.testing.assert_allclose(adata.var["means"], dask_data.var["means"])
