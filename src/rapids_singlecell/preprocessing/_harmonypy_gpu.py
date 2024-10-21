a3a8c34d32fcd809bfe0b90c4956088eb3c23fa6
Intron7 <severin.dicks@icloud.com>
2024-10-21 16:03:01 +0200

    update

--- src/rapids_singlecell/preprocessing/_harmonypy_gpu.py
+++ src/rapids_singlecell/preprocessing/_harmonypy_gpu.py
@@ -1,3 +1,20 @@
+# harmonypy - A data alignment algorithm.
+# Copyright (C) 2018  Ilya Korsunsky
+#               2019  Kamil Slowikowski <kslowikowski@gmail.com>
+#               2022  Severin Dicks <severin.dicks@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import logging
@@ -16,9 +33,8 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+
 # from IPython.core.debugger import set_trace
-
-
 def run_harmony(
     data_mat: np.ndarray,
     meta_data: pd.DataFrame,
