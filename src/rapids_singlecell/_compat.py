5aace0debdc6f75262da34f410ea3373da2e2dd6
Intron7 <severin.dicks@icloud.com>
2024-12-19 17:14:19 +0100

    update anndata to XPU for dask

--- src/rapids_singlecell/_compat.py
+++ src/rapids_singlecell/_compat.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
 import cupy as cp
+import numpy as np
 from cupyx.scipy.sparse import csr_matrix
 from dask.array import Array as DaskArray  # noqa: F401
+from scipy.sparse import csc_matrix as csc_matrix_cpu
+from scipy.sparse import csr_matrix as csr_matrix_cpu
 
 
 def _meta_dense(dtype):
@@ -11,3 +14,23 @@ def _meta_dense(dtype):
 
 def _meta_sparse(dtype):
     return csr_matrix(cp.array((1.0,), dtype=dtype))
+
+
+def _meta_dense(dtype):
+    return cp.zeros([0], dtype=dtype)
+
+
+def _meta_sparse(dtype):
+    return csr_matrix(cp.array((1.0,), dtype=dtype))
+
+
+def _meta_dense_cpu(dtype):
+    return np.zeros([0], dtype=dtype)
+
+
+def _meta_sparse_csr_cpu(dtype):
+    return csr_matrix_cpu(np.array((1.0,), dtype=dtype))
+
+
+def _meta_sparse_csc_cpu(dtype):
+    return csc_matrix_cpu(np.array((1.0,), dtype=dtype))
