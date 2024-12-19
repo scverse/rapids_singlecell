8e3fef152aa312b910ebfc8196467a512ccd32a5
Severin Dicks <37635888+Intron7@users.noreply.github.com>
2024-12-19 17:30:18 +0100

    Add anndata to gpu for dask (#312)
    * update anndata to XPU for dask
    
    * update test

--- src/rapids_singlecell/get/_anndata.py
+++ src/rapids_singlecell/get/_anndata.py
@@ -7,11 +7,20 @@ import cupy as cp
 import numpy as np
 from cupyx.scipy.sparse import csc_matrix as csc_matrix_gpu
 from cupyx.scipy.sparse import csr_matrix as csr_matrix_gpu
+from dask.array import Array as DaskArray
 from scanpy.get import _get_obs_rep, _set_obs_rep
 from scipy.sparse import csc_matrix as csc_matrix_cpu
 from scipy.sparse import csr_matrix as csr_matrix_cpu
 from scipy.sparse import isspmatrix_csc as isspmatrix_csc_cpu
 from scipy.sparse import isspmatrix_csr as isspmatrix_csr_cpu
+
+from rapids_singlecell._compat import (
+    _meta_dense,
+    _meta_dense_cpu,
+    _meta_sparse,
+    _meta_sparse_csc_cpu,
+    _meta_sparse_csr_cpu,
+)
 
 if TYPE_CHECKING:
     from anndata import AnnData
@@ -79,6 +88,11 @@ def X_to_GPU(X: CPU_ARRAY_TYPE, warning: str = "X") -> GPU_ARRAY_TYPE:
     """
     if isinstance(X, GPU_ARRAY_TYPE):
         pass
+    elif isinstance(X, DaskArray):
+        if isinstance(X._meta, csc_matrix_cpu):
+            pass
+        meta = _meta_sparse if isinstance(X._meta, csr_matrix_cpu) else _meta_dense
+        X = X.map_blocks(X_to_GPU, meta=meta(X.dtype))
     elif isspmatrix_csr_cpu(X):
         X = csr_matrix_gpu(X)
     elif isspmatrix_csc_cpu(X):
@@ -146,6 +160,14 @@ def X_to_CPU(X: GPU_ARRAY_TYPE) -> CPU_ARRAY_TYPE:
     X
         Matrix or array to transfer to the host memory
     """
+    if isinstance(X, DaskArray):
+        if isinstance(X._meta, csr_matrix_gpu):
+            meta = _meta_sparse_csr_cpu
+        elif isinstance(X._meta, csc_matrix_gpu):
+            meta = _meta_sparse_csc_cpu
+        else:
+            meta = _meta_dense_cpu
+        X = X.map_blocks(X_to_GPU, meta=meta(X.dtype))
     if isinstance(X, GPU_ARRAY_TYPE):
         X = X.get()
     else:
