5ec48642e5ead6c2a9e80bf9c2519f597dae839c
Intron7 <severin.dicks@icloud.com>
2025-03-03 14:24:39 +0100

    update notebooks

--- tests/test_neighbors.py
+++ tests/test_neighbors.py
@@ -32,7 +32,7 @@ connectivities_umap = [
 ]
 
 
-@pytest.mark.parametrize("algo", ["brute", "cagra", "ivfflat", "nn_descent"])
+@pytest.mark.parametrize("algo", ["brute", "cagra", "ivfflat"])
 def test_umap_connectivities_euclidean(algo):
     adata = AnnData(X=X)
     neighbors(adata, n_neighbors=3, algorithm=algo)
