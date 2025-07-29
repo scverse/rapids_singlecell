2d9104b33b13cc1586bb7069615cc3e1fb261cf7
Intron7 <severin.dicks@icloud.com>
2025-07-29 17:38:44 +0200

    clean up X_to _GPU

--- src/rapids_singlecell/get/_anndata.py
+++ src/rapids_singlecell/get/_anndata.py
@@ -91,11 +91,7 @@ def X_to_GPU(
     if isinstance(X, GPU_ARRAY_TYPE):
         pass
     elif isinstance(X, DaskArray):
-        if isinstance(X._meta, csc_matrix_cpu):
-            pass
-        elif isinstance(X._meta, csr_matrix_gpu) or isinstance(X._meta, cp.ndarray):
-            pass
-        else:
+        if isinstance(X._meta, csr_matrix_cpu) or isinstance(X._meta, np.ndarray):
             meta = _meta_sparse if isinstance(X._meta, csr_matrix_cpu) else _meta_dense
             X = X.map_blocks(X_to_GPU, meta=meta(X.dtype))
     elif isspmatrix_csr_cpu(X):
