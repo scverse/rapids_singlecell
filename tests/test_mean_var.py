a8f2f0f644fcabcaa9b21b84da08e492b45277cf
Intron7 <severin.dicks@icloud.com>
2025-08-08 14:45:40 +0200

    speed up mean_var * update harmony
    * fix release notes

---
+++ tests/test_mean_var.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import cupy as cp
+import numpy as np
+import pytest
+from scanpy.datasets import pbmc3k, pbmc68k_reduced
+from scanpy.preprocessing._utils import _get_mean_var as sc_get_mean_var
+
+import rapids_singlecell as rsc
+from rapids_singlecell.preprocessing._utils import _get_mean_var as rsc_get_mean_var
+
+
+@pytest.mark.parametrize("data_kind", ["csc", "csr", "dense"])
+@pytest.mark.parametrize("axis", [0, 1])
+@pytest.mark.parametrize("dtype", [np.float32, np.float64])
+def test_mean_var(data_kind, axis, dtype):
+    if data_kind == "dense":
+        adata = pbmc68k_reduced()
+        adata.X = adata.X.astype(dtype)
+        cudata = rsc.get.anndata_to_GPU(adata, copy=True)
+    else:
+        adata = pbmc3k()
+        if data_kind == "csc":
+            adata.X = adata.X.tocsc()
+        adata.X = adata.X.astype(dtype)
+        cudata = rsc.get.anndata_to_GPU(adata, copy=True)
+
+    mean, var = sc_get_mean_var(adata.X, axis=axis)
+    rsc_mean, rsc_var = rsc_get_mean_var(cudata.X, axis=axis)
+
+    cp.testing.assert_allclose(mean, rsc_mean)
+    cp.testing.assert_allclose(var, rsc_var)
