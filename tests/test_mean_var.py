2e7f34b0cdde16f1eac7c4e5cefb128ca94f7a5f
Intron7 <severin.dicks@icloud.com>
2025-08-08 14:58:55 +0200

    fix test

--- tests/test_mean_var.py
+++ tests/test_mean_var.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import cupy as cp
 import numpy as np
 import pytest
+from fast_array_utils.stats import mean_var as sc_get_mean_var
 from scanpy.datasets import pbmc3k, pbmc68k_reduced
-from scanpy.preprocessing._utils import _get_mean_var as sc_get_mean_var
 
 import rapids_singlecell as rsc
 from rapids_singlecell.preprocessing._utils import _get_mean_var as rsc_get_mean_var
@@ -25,7 +25,7 @@ def test_mean_var(data_kind, axis, dtype):
         adata.X = adata.X.astype(dtype)
         cudata = rsc.get.anndata_to_GPU(adata, copy=True)
 
-    mean, var = sc_get_mean_var(adata.X, axis=axis)
+    mean, var = sc_get_mean_var(adata.X, axis=axis, correction=1)
     rsc_mean, rsc_var = rsc_get_mean_var(cudata.X, axis=axis)
 
     cp.testing.assert_allclose(mean, rsc_mean)
