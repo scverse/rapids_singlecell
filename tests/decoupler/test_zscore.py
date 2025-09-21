1c06838bc0b4780a0d1ca2a2c7741142615ad9f8
sviatkh <sviatoslavkharuk@gmail.com>
2025-09-21 10:48:00 +0000

    zscore implementation with CuPy

---
+++ tests/decoupler/test_zscore.py
@@ -0,0 +1,19 @@
+import cupy as cp
+import numpy as np
+import pytest
+
+import rapids_singlecell.decoupler_gpu as dc
+
+
+@pytest.mark.parametrize("flavor", ["KSEA", "RoKAI"])
+def test_func_zscore(
+    mat,
+    adjmat,
+    flavor,
+):
+    X, obs, var = mat
+    X_cp = cp.array(X)
+    adjmat_cp = cp.array(adjmat)
+    es, pv = dc._method_zscore._func_zscore(mat=X_cp, adj=adjmat_cp, flavor=flavor)
+    assert np.isfinite(es).all()
+    assert ((0 <= pv) & (pv <= 1)).all()
