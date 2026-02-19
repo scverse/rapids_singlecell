284039ef0f20384960ca104d77cf253e84e521f1
Intron7 <severin.dicks@icloud.com>
2026-02-19 14:10:38 +0100

    add cuda-12 docker

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -3,10 +3,34 @@ set -euxo pipefail
 
 docker_account=scverse
 rapids_version=26.02
-grep -v -- '- rapids-singlecell' conda/rsc_rapids_${rapids_version}_cuda13.yml > rsc_rapids.yml
-docker build -t rapids-singlecell-deps:latest -f docker/Dockerfile.deps .
-rm rsc_rapids.yml
-docker build -t rapids-singlecell:latest -f docker/Dockerfile .
-latest_id=$( docker images |grep -e "rapids-singlecell[ \t]*latest"|head -n1|awk '{print $3}' )
-#docker tag $latest_id $docker_account/rapids-singlecell:latest
-#docker push $docker_account/rapids-singlecell:latest
+
+declare -A cuda_versions=(
+    [cuda12]="12.9.0"
+    [cuda13]="13.0.2"
+)
+
+declare -A cuda_archs=(
+    [cuda12]="75-real;80-real;86-real;89-real;90-real"
+    [cuda13]="75-real;80-real;86-real;89-real;90-real;100-real;120"
+)
+
+for label in cuda12 cuda13; do
+    ver=${cuda_versions[$label]}
+
+    grep -v -- '- rapids-singlecell' conda/rsc_rapids_${rapids_version}_${label}.yml > docker/rsc_rapids.yml
+    docker build \
+        --build-arg CUDA_VER="${ver}" \
+        -t rapids-singlecell-deps:latest-${label} \
+        -f docker/Dockerfile.deps \
+        docker/
+    rm docker/rsc_rapids.yml
+
+    docker build \
+        --build-arg CUDA_ARCHS="${cuda_archs[$label]}" \
+        --build-context rapids-singlecell-deps=docker-image://rapids-singlecell-deps:latest-${label} \
+        -t rapids-singlecell:latest-${label} \
+        -f docker/Dockerfile \
+        docker/
+done
+
+docker image ls -a
